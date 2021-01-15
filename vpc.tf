5d4d7a81e837dce4ac4fd5e7ba0944011fad6775
optimumsun <optimumsun@users.noreply.github.com>
2021-01-14 22:00:11 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -12,6 +12,7 @@ provider "ibm" {
 }
 
 locals {
+  SSHKEY = "schematics"
   BASENAME = "schematics"
   ZONE     = "us-south-1"
 }
@@ -61,7 +62,7 @@ resource ibm_is_instance "vsi1" {
   resource_group = "data.ibm_resource_group.group.id"
   vpc     = ibm_is_vpc.vpc.id
   zone    = local.ZONE
-  keys    = [data.ibm_is_ssh_key.ssh_key_id.id]
+  keys    = [local.SSHKEY]
   image   = data.ibm_is_image.ubuntu.id
   profile = "cc1-2x4"
 
