dc2104678c4eda802184157a5b929ab74236a850
optimumsun <optimumsun@users.noreply.github.com>
2021-01-14 21:40:33 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -21,7 +21,7 @@ resource ibm_is_vpc "vpc" {
 }
 
 resource ibm_is_security_group "sg1" {
-  name = "locals.BASENAME-sg1"
+  name = "${local.BASENAME}-sg1"
   vpc  = ibm_is_vpc.vpc.id
 }
 
@@ -38,9 +38,9 @@ resource "ibm_is_security_group_rule" "ingress_ssh_all" {
 }
 
 resource ibm_is_subnet "subnet1" {
-  name = "locals.BASENAME-subnet1"
+  name = "${local.BASENAME}-subnet1"
   vpc  = ibm_is_vpc.vpc.id
-  zone = "locals.ZONE"
+  zone = local.ZONE
   total_ipv4_address_count = 256
 }
 
@@ -57,10 +57,10 @@ data ibm_resource_group "group" {
 }
 
 resource ibm_is_instance "vsi1" {
-  name    = "locals.BASENAME-vsi1"
+  name    = "${local.BASENAME}-vsi1"
   resource_group = "data.ibm_resource_group.group.id"
   vpc     = ibm_is_vpc.vpc.id
-  zone    = "locals.ZONE"
+  zone    = local.ZONE
   keys    = [data.ibm_is_ssh_key.ssh_key_id.id]
   image   = data.ibm_is_image.ubuntu.id
   profile = "cc1-2x4"
@@ -72,7 +72,7 @@ resource ibm_is_instance "vsi1" {
 }
 
 resource ibm_is_floating_ip "fip1" {
-  name   = "locals.BASENAME-fip1"
+  name   = "${local.BASENAME}-fip1"
   target = ibm_is_instance.vsi1.primary_network_interface.0.id
 }
 
